d8579532607578233d3755401ec002f601f20963
amaydixit11 <amaydixit11@gmail.com>
2025-09-11 01:36:54 +0530

    projects removed from home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Hero from "@/components/Hero";
-import { ProjectsGrid } from "@/components/ProjectsGrid";
+// import { ProjectsGrid } from "@/components/ProjectsGrid";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <ProjectsGrid /> {/* New section added */}
+      {/* <ProjectsGrid /> New section added */}
     </>
   );
 }
