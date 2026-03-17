f3a0e25d0da311a5d7f647dffcfc28738677de0f
Amay Dixit <65495380+amaydixit11@users.noreply.github.com>
2026-03-17 16:47:10 +0000

    build issues fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 import createMDX from '@next/mdx';
-import remarkGfm from 'remark-gfm';
+// import remarkGfm from 'remark-gfm';
 
 const nextConfig: NextConfig = {
   images: {
@@ -24,7 +24,7 @@ const nextConfig: NextConfig = {
 
 const withMDX = createMDX({
   options: {
-    remarkPlugins: [remarkGfm],
+    remarkPlugins: [],
     rehypePlugins: [],
   },
 });
